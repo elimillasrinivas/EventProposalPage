7b1cffa7bf28424c6af81326f05984210ce85853
uttej-varma <122285478+uttej-varma@users.noreply.github.com>
2023-03-02 19:47:30 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const proposalRouter = require("./routes/proposal");
-const vendorRouter = require("./routes/Vendor");
-const userRouter = require("./routes/user");
+const vendorRouter = require("./routes/vendor");
+const userRouter = require("./routes/User");
 const cookieParser = require("cookie-parser");
 const conn=require("./connection/connect");
 conn();//connection with backend established
