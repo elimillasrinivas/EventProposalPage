3756afd3aeb1f32e27a49b292ab3273a5a7fea92
uttej varma <uttej2233@gmail.com>
2023-03-02 16:38:52 +0530

    done eventinfo,landingpage,landingpagelistitem

---
+++ client/src/components/eventInfo/eventInfo.jsx
@@ -0,0 +1,126 @@
+import TopBar from "../topBar/topbar";
+import {ChevronLeft,ArrowBack} from "@mui/icons-material";
+import axios from "axios";
+import "./eventInfo.css";
+// import {smallData} from "../../data"
+
+const EventInfo=(props)=>{
+    const closeEventInfoHandler=()=>{
+        props.close[props.ind]=false;
+        props.setClose([...props.close]);
+      }
+    const selectEventHandler=()=>{
+        if(props.select.isValid===false)
+        {
+            
+           
+            axios.put(`http://localhost:8000/users/${props.user}`,{select:props.show._id})
+            .then((response)=>{
+                axios.get(`http://localhost:8000/events/${props.show._id}`,{withCredentials:true})
+                .then((res)=>{
+                    props.setSelect({isValid:true,data:res.data.result})
+                    
+                    console.log(res.data.result);
+                    closeEventInfoHandler();
+                    
+                }).catch((e)=>{console.log(e)})
+            })
+                                 
+            .catch((e)=>{console.log(e)})
+            
+
+        }
+        else{
+            alert("user can select only one Item")
+        }
+    }
+    return(
+        <>
+        <article className="eventInfoContainer">
+            <TopBar/>
+            <header>
+                <section>
+                    <h4>Proposals<ChevronLeft/></h4>
+                    <h4>Contract type</h4>
+                </section>
+                <section>
+                    <ArrowBack onClick={closeEventInfoHandler} className="eventInfopageexit"/>
+                    <button onClick={selectEventHandler}>Select</button>
+                </section>
+            </header>
+            <section className="eventInfoBodyContainer">
+               <section className="eventInfoBodyLeftPart">
+                  <section>
+                    <img src={props.show.eventImage} alt="pic"/>
+                    <h4>ID:<span>0001</span></h4>
+                    <ul>
+                        <li><h4>Name</h4><span>{props.show.name}</span></li>
+                        <li><h4>email</h4><span>{props.show.vendorEmail}</span></li>
+                        <li>
+                            <section>
+                                <h4>Start Date:</h4><span>{props.show.from}</span>
+                                <h4>End Date:</h4><span>{props.show.to}</span>
+                            </section>
+                        </li>
+                        <li>
+                            <section>
+                                <h4>Event Type:</h4>
+                                <p>{props.show.eventType}</p>
+                            </section>
+                            <section>
+                                <h4>Event Class:</h4>
+                                <p>{props.show.budget<50000?"Class B":"Class A"}</p>
+                            </section>
+                        </li>
+                    </ul>
+                  </section>
+                  <section>
+                  <h4>My Albums</h4>
+                  <section>
+                    {
+                        props.show.venueImage.map((value,index)=>{
+                             return(
+                                <img src={value} key={index} alt="pic"/>
+                             )
+                        })
+                    }
+                  </section>
+
+               </section>
+               </section>
+               
+               <section className="eventInfoBodyRightPart">
+                  <section>
+                    <h4>Venue and Arrangements</h4>
+                    <p>
+                        {props.show.description}
+                    </p>
+                  </section>
+                  <section>
+                    <h4>Food Preferences</h4>
+                    <p>
+                        {props.show.foodPreferences}
+                    </p>
+                  </section>
+                  <section>
+                    <h4>Contacts | 12</h4>
+                    <ul>
+                        <li><h5>Contact 1</h5><span>+91 xxxxxxxxxx</span></li>
+                        <li><h5>Contact 2</h5><span>+91 xxxxxxxxxx</span></li>
+                        <li><h5>Contact 3</h5><span>+91 xxxxxxxxxx</span></li>
+                    </ul>
+                  </section>
+                  <section>
+                    <h4>Events</h4>
+                    <p>
+                        {props.show.events}
+                    </p>
+                  </section>
+               </section>
+            </section>
+        </article>
+
+        </>
+    )
+}
+export default EventInfo;
